7a80a3095287f323ef061ca38cac82cf3009416e
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2021-12-21 23:41:43 +0200

    Update AssociationTable.java

--- AssociationTable.java
+++ AssociationTable.java
@@ -1,40 +1,43 @@
-import jdk.nashorn.internal.objects.annotations.Constructor;
-
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.TreeMap;
 
-public class AssociationTable<K,V> implements Comparable<K> {
-
-    //attributes
-    private List<K> _key;
-    private List<V> _value;
+public class AssociationTable<K extends Comparable,V> extends TreeMap {
 
     //constructor
-    public AssociationTable(){}
-    public AssociationTable(K key,V value){
-        _key.add(key);
-        _value.add(value);
+    public AssociationTable(){super();}
+    public AssociationTable(K[] keys, V[] valus){
+
+        int i=0;
+        while( !keys[i].equals(null)){i++;}
+
     }
 
-    //get & set
-    public List<K> getKeys() {return _key;}
-    public K getKey(int i){return _key.get(i);}
-    public List<V> getValues() {return _value;}
-    public V getValue(int i){return _value.get(i);}
-
-    //public <V> get(K){retrun _value[0];}
-
     //methods
+    public void add(K key, V value){ //add value to table
+        super.put(key,value);
+    }
+
+    public boolean contains(K key){
+        return super.containsKey(key);
+    }
+
+
     //public void add(K,V){}
     //public <V> get(<K>){}
     //public boolean contains(){}
     //public K remove(){}
 
+
+
     public static void main(String[] args) {
-        AssociationTable<Integer,String> t1 = new AssociationTable();
-        AssociationTable t2 = new AssociationTable(4,"name");
+        AssociationTable t1 = new AssociationTable();
+        t1.add(0,"sdfgsdf");
+        t1.add(7,"yui");
+        t1.add(8,"vbn");
+        t1.add(3,"qew");
+        t1.add(1,"nmui");
+        System.out.println( t1.contains(0));
+        //AssociationTable t2 = new AssociationTable(4,"name");
         System.out.println("Hello World!");
     }
-
 }
