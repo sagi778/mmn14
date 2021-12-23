da47c920f5be74f2e7da4c0fbd0a2ce0e3101a86
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2021-12-23 10:51:54 +0200

    Update AssociationTable.java

--- AssociationTable.java
+++ AssociationTable.java
@@ -1,20 +1,18 @@
 import java.util.Iterator;
-import java.util.List;
+import java.util.Arrays;
 import java.util.TreeMap;
 
 public class AssociationTable<K extends Comparable,V> extends TreeMap {
 
     //constructor
     public AssociationTable(){super();}
-    public AssociationTable(K[] keys, V[] values){///////////////////////////////////
+    public AssociationTable(K[] keys, V[] values){
 
-        int i=0;
-        while( !keys[i].equals(null)){
+        int size = keys.length;
 
+        for(int i=0; i < keys.length; i++){
             add(keys[i],values[i]);
-            i++;
         }
-
     }
 
     //methods
@@ -40,22 +38,25 @@ public class AssociationTable<K extends Comparable,V> extends TreeMap {
     public int size(){return super.size();}
     public Iterator KeyIterator(){
 
-        //super.floorKey()
+        return (Arrays.asList( super.keySet() )).iterator();
     }
 
     public static void main(String[] args) {
 
-        Integer[] keys = {0,1,2,4};
+        Integer[] keys = {0,13,25,4};
         String[] values = {"qwe","gf","mnb","ytu"};
 
-        //AssociationTable t2 = new AssociationTable(keys,values);
+        AssociationTable t2 = new AssociationTable(keys,values);
+        System.out.println( t2 );
 
         AssociationTable t1 = new AssociationTable();
+
         t1.add(0,"sdfgsdf");
         t1.add(7,"yui");
         t1.add(8,"vbn");
         t1.add(3,"qew");
         t1.add(1,"nmui");
+        System.out.println( t1 );
         System.out.println( t1.contains(0));
         int sz = t1.size();
         t1.remove(3);
